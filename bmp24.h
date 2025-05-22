53b294d4cb89b48ca8fa19c5c92c833609039967
Camille.Lubrano <Camille.lubrano-lavaderci@efrei.net>
2025-05-22 17:35:48 +0200

    bmp24.c updated

--- bmp24.h
+++ bmp24.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 
 // Offsets for the BMP header
 #define BITMAP_MAGIC 0x00 // offset 0
@@ -13,17 +14,20 @@
 #define BITMAP_HEIGHT 0x16 // offset 22
 #define BITMAP_DEPTH 0x1C // offset 28
 #define BITMAP_SIZE_RAW 0x22 // offset 34
+
 // Magical number for BMP files
 #define BMP_TYPE 0x4D42 // 'BM' in hexadecimal
+
 // Header sizes
 #define HEADER_SIZE 0x0E // 14 octets
 #define INFO_SIZE 0x28 // 40 octets
+
 // Constant for the color depth
 #define DEFAULT_DEPTH 0x18 // 24
 
 typedef struct {
 	t_bmp_header header;
-	t_bmp_info header_info;
+ 	t_bmp_info header_info;
 	int width;
 	int height;
 	int colorDepth;
@@ -34,7 +38,7 @@ typedef struct {
 	uint16_t type;
 	uint32_t size;
 	uint16_t reserved1;
-	uint16_t reserved2;
+ 	uint16_t reserved2;
 	uint32_t offset;
 } t_bmp_header;
 
@@ -58,11 +62,37 @@ typedef struct {
 	uint8_t blue;
 } t_pixel;
 
+//2.3
 t_pixel ** bmp24_allocateDataPixels (int width, int height);
 void bmp24_freeDataPixels (t_pixel ** pixels, int height);
 
 t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
 void bmp24_free (t_bmp24 * img);
 
+
+//2.4
 t_bmp24 * bmp24_loadImage (const char * filename);
 void bmp24_saveImage (t_bmp * img, const char * filename);
+
+void file_rawRead (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+	fseek(file, position, SEEK_SET);
+	fread(buffer, size, n, file);
+}
+
+void file_rawWrite (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file) {
+	fseek(file, position, SEEK_SET);
+	fwrite(buffer, size, n, file);
+}
+
+void bmp24_readPixelValue (t_bmp * image, int x, int y, FILE * file);
+void bmp24_readPixelData (t_bmp * image, FILE * file);
+void bmp24_writePixelValue (t_bmp * image, int x, int y, FILE * file);
+void bmp24_writePixelData (t_bmp * image, FILE * file);
+
+//bmp24_loadImage
+//bmp24_saveImage
+//bmp24_
+
+
+
+
