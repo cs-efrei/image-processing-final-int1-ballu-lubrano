081aecba640f97656911fb2a0e6d947582390fca
Camille.Lubrano <Camille.lubrano-lavaderci@efrei.net>
2025-05-25 14:57:27 +0200

    bmp24.h finished

--- bmp24.h
+++ bmp24.h
@@ -19,15 +19,6 @@
 #define DEFAULT_DEPTH 0x18 // 24
 
 typedef struct {
-	t_bmp_header header;
- 	t_bmp_info header_info;
-	int width;
-	int height;
-	int colorDepth;
-	t_pixel **data;
-} t_bmp24;
-
-typedef struct {
 	uint16_t type;
 	uint32_t size;
 	uint16_t reserved1;
@@ -55,25 +46,48 @@ typedef struct {
 	uint8_t blue;
 } t_pixel;
 
+typedef struct {
+	t_bmp_header header;
+ 	t_bmp_info header_info;
+	int width;
+	int height;
+	int colorDepth;
+	t_pixel **data;
+} t_bmp24;
+
 //2.3 Allocation and deallocation functions
 t_pixel ** bmp24_allocateDataPixels (int width, int height);
 void bmp24_freeDataPixels (t_pixel ** pixels, int height);
+
 t_bmp24 * bmp24_allocate (int width, int height, int colorDepth);
 void bmp24_free (t_bmp24 * img);
 
 
 //2.4 Features: Loading and Saving 24-bit Images
 t_bmp24 * bmp24_loadImage (const char * filename);
-void bmp24_saveImage (t_bmp * img, const char * filename);
+void bmp24_saveImage (t_bmp24 * img, const char * filename);
 
 void file_rawRead (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file);
 void file_rawWrite (uint32_t position, void * buffer, uint32_t size, size_t n, FILE * file);
 
-void bmp24_readPixelValue (t_bmp * image, int x, int y, FILE * file);
-void bmp24_readPixelData (t_bmp * image, FILE * file);
-void bmp24_writePixelValue (t_bmp * image, int x, int y, FILE * file);
-void bmp24_writePixelData (t_bmp * image, FILE * file);
+void bmp24_readPixelValue (t_bmp24 * image, int x, int y, FILE * file);
+void bmp24_readPixelData (t_bmp24 * image, FILE * file);
+void bmp24_writePixelValue (t_bmp24 * image, int x, int y, FILE * file);
+void bmp24_writePixelData (t_bmp24 * image, FILE * file);
 
-//2.4 unfinished
+t_bmp24 *bmp24_loadImage(const char *filename);
 
+void bmp24_saveImage(t_bmp24 *img, const char *filename);
 
+void bmp24_negative(t_bmp24 *img);
+void bmp24_grayscale(t_bmp24 *img);
+void bmp24_brightness(t_bmp24 *img, int value);
+
+t_pixel bmp24_convolution(t_bmp24 *img, int x, int y, float **kernel, int kernelSize);
+void bmp24_boxBlur(t_bmp24 *img);
+void bmp24_gaussianBlur(t_bmp24 *img);
+void bmp24_outline(t_bmp24 *img);
+void bmp24_emboss(t_bmp24 *img);
+void bmp24_sharpen(t_bmp24 *img);
+
+#endif
